7fc62e9edf0870d486273fac34fbc1fecc44dd70
Lucas <lucj765.27@gmail.com>
2026-04-17 10:18:15 -0300

    atualizar email e insta

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,28 @@
+import { MessageCircle } from 'lucide-react'; // Importa o ícone de conversa
 function Footer() {
   return (
     <footer className="rodape">
            <div className="rodape-container">
         <div className="rodape-col">
             <h3>Contato</h3>
-            <p>📞 (31) 92009-2519</p>
-            <p>📧 contato@gomesmarcenaria.com.br</p>
+            <p>
+          <MessageCircle size={20} color="#25D366" /> {/* Ícone do Zap com a cor oficial */}
+          (31) 92009-2519
+          
+        </p>
+            <p>📧 comercial@twrplanejados.com.br</p>
         </div>
 
         <div className="rodape-col">
             <h3>Endereço</h3>
             <p>Belo Horizonte - MG</p>
             <p>Rua Gilberto Freire, 157 - Bairro Bom Sucesso</p>
-            <p>CNPJ: 00.000.000/0001-00</p>
+            
         </div>
 
         <div className="rodape-col">
             <h3>Siga-nos</h3>
-            <a href="https://instagram.com/seuusuario" target="_blank" className="link-social">Instagram</a>
+            <a href="https://www.instagram.com/wogs_moveispersonalizados?igsh=aGthcmVuNzh4c21r" target="_blank" className="link-social">Instagram</a>
         </div>
     </div>
     <div className="rodape-direitos">
